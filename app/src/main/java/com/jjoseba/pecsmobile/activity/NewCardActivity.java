891a33c8dd9dc59fba0b32077d68e506636e10f5
Joseba <jjoseba@gmail.com>
2015-01-23 13:23:41 +0100

    Implemented color picker and view transitions

--- app/src/main/java/com/jjoseba/pecsmobile/activity/NewCardActivity.java
+++ app/src/main/java/com/jjoseba/pecsmobile/activity/NewCardActivity.java
@@ -1,31 +1,45 @@
 package com.jjoseba.pecsmobile.activity;
 
+import android.animation.ArgbEvaluator;
+import android.animation.ValueAnimator;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.jjoseba.pecsmobile.R;
+import com.jjoseba.pecsmobile.model.CardPECS;
 import com.larswerkman.holocolorpicker.ColorPicker;
 import com.larswerkman.holocolorpicker.OpacityBar;
 import com.larswerkman.holocolorpicker.SVBar;
-import com.larswerkman.holocolorpicker.ColorPicker.OnColorChangedListener;
+import com.larswerkman.holocolorpicker.ColorPicker.OnColorSelectedListener;
+import com.larswerkman.holocolorpicker.SaturationBar;
+import com.larswerkman.holocolorpicker.ValueBar;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
-public class NewCardActivity extends Activity implements OnColorChangedListener {
+public class NewCardActivity extends Activity{
+
+    private static float EXTRA_TRANSLATION = 300f;
+    private static long ANIM_DURATION = 800;
 
     private ColorPicker picker;
-    private SVBar svBar;
-    private OpacityBar opacityBar;
-    private Button button;
-    private TextView text;
+    private View colorPickerContainer;
+    private View cardFrame;
+    private View colorBucket;
+    private int previousColor = CardPECS.DEFAULT_COLOR;
+
+    private boolean disableOkButton = false;
 
     @Override
     protected void attachBaseContext(Context newBase) {
@@ -45,50 +59,118 @@ public class NewCardActivity extends Activity implements OnColorChangedListener 
             bar.setBackgroundDrawable(new ColorDrawable(cardColor));
         }
 
-        picker = (ColorPicker) findViewById(R.id.picker);
-        svBar = (SVBar) findViewById(R.id.svbar);
-        opacityBar = (OpacityBar) findViewById(R.id.opacitybar);
-        button = (Button) findViewById(R.id.button1);
-        text = (TextView) findViewById(R.id.textView1);
+        cardFrame = findViewById(R.id.card_frame);
+        colorBucket = findViewById(R.id.colorBucket);
 
-        picker.addSVBar(svBar);
-        picker.addOpacityBar(opacityBar);
-        picker.setOnColorChangedListener(this);
-
-        button.setOnClickListener(new View.OnClickListener() {
-
+        colorPickerContainer = findViewById(R.id.pickerContainer);
+        colorPickerContainer.setOnTouchListener(new View.OnTouchListener() {
+            //Cancelamos la propagación del pulsado cuando colorPickerContainer es visible
             @Override
-            public void onClick(View v) {
-                text.setTextColor(picker.getColor());
-                picker.setOldCenterColor(picker.getColor());
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                return (colorPickerContainer.getVisibility() == View.VISIBLE);
             }
         });
-    }
 
-    @Override
-    public void onColorChanged(int color) {
-        //gives the color when it's changed.
+        picker = (ColorPicker) colorPickerContainer.findViewById(R.id.picker);
+        picker.addSaturationBar((SaturationBar) colorPickerContainer.findViewById(R.id.saturationbar) );
+        picker.addValueBar((ValueBar) colorPickerContainer.findViewById(R.id.valuebar));
+
+        Button selectColorBtn = (Button) colorPickerContainer.findViewById(R.id.select_color_btn);
+        selectColorBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int selectedColor = picker.getColor();
+                picker.setOldCenterColor(selectedColor);
+                hideColorPicker();
+                changeColor(selectedColor);
+            }
+        });
+
+        ImageButton pickColor = (ImageButton) findViewById(R.id.pickColorButton);
+        pickColor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showColorPicker();
+            }
+        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_new_card, menu);
+
         return true;
+    }
+
+    public boolean onPrepareOptionsMenu(Menu menu){
+        menu.getItem(0).setEnabled(!disableOkButton);
+        return true;
+    }
+
+    private void showColorPicker(){
+        toggleColorPicker(true);
+        disableOkButton = true;
+        invalidateOptionsMenu();
+    }
+
+    private void hideColorPicker(){
+        toggleColorPicker(false);
+        disableOkButton = false;
+        invalidateOptionsMenu();
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_done) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (colorPickerContainer.getVisibility() != View.VISIBLE){
+            super.onBackPressed();
+        }
+        else{
+            hideColorPicker();
+        }
+    }
+
+    protected void changeColor(int colorTo){
+        int colorFrom = previousColor;
+        ValueAnimator colorAnimation = ValueAnimator.ofObject(new ArgbEvaluator(), colorFrom, colorTo);
+        colorAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+
+            @Override
+            public void onAnimationUpdate(ValueAnimator animator) {
+                cardFrame.setBackgroundColor((Integer)animator.getAnimatedValue());
+                colorBucket.setBackgroundColor((Integer)animator.getAnimatedValue());
+            }
+        });
+        colorAnimation.setDuration(ANIM_DURATION).start();
+        previousColor = colorTo;
+    }
+    protected void toggleColorPicker(boolean show){
+
+        float translation = - colorPickerContainer.getMeasuredHeight() - EXTRA_TRANSLATION;
+        float initY  =  show ? translation : 0f;
+        float finalY = !show ? translation : 0f;
+
+        TranslateAnimation anim = new TranslateAnimation(
+                TranslateAnimation.ABSOLUTE, 0f,
+                TranslateAnimation.ABSOLUTE, 0f,
+                TranslateAnimation.ABSOLUTE, initY,
+                TranslateAnimation.ABSOLUTE, finalY);
+        anim.setDuration(ANIM_DURATION);
+        anim.setFillAfter(false);
+
+        if (show) { colorPickerContainer.setVisibility(View.VISIBLE); }
+        colorPickerContainer.startAnimation(anim);
+        if (!show) { colorPickerContainer.setVisibility(View.INVISIBLE); }
+
     }
 }
