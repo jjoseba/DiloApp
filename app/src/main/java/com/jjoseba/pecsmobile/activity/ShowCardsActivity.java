762a5547e31196c36570033d215e206d8fdb055a
Joseba <jjoseba@gmail.com>
2015-05-06 11:56:02 +0200

    Showing results nicely

--- app/src/main/java/com/jjoseba/pecsmobile/activity/ShowCardsActivity.java
+++ app/src/main/java/com/jjoseba/pecsmobile/activity/ShowCardsActivity.java
@@ -1,14 +1,18 @@
 package com.jjoseba.pecsmobile.activity;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Context;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.WindowManager;
 import android.widget.GridView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.jjoseba.pecsmobile.R;
 import com.jjoseba.pecsmobile.adapter.CardGridAdapter;
@@ -35,15 +39,30 @@ public class ShowCardsActivity extends Activity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_show_cards);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getActionBar().setDisplayHomeAsUpEnabled(false);
-        getActionBar().setHomeButtonEnabled(false);
 
         Bundle extras = getIntent().getExtras();
         selectedCards = (ArrayList<CardPECS>) extras.getSerializable("result");
         String title = "";
         for (CardPECS card : selectedCards){ title += card.getLabel() + " ";  }
-        getActionBar().setTitle(title);
+
+        ActionBar actionBar = getActionBar();
+        if (actionBar != null){
+            getActionBar().setDisplayHomeAsUpEnabled(false);
+            getActionBar().setHomeButtonEnabled(false);
+            getActionBar().setTitle(title);
+        }
+
         cardsList = (GridView) findViewById(R.id.selectedCards);
+        if (selectedCards.size() == 2){
+            cardsList.setNumColumns(2);
+        }
+        else if (selectedCards.size() == 1){
+            cardsList.setNumColumns(1);
+            DisplayMetrics metrics = new DisplayMetrics();
+            getWindowManager().getDefaultDisplay().getMetrics(metrics);
+            cardsList.setPadding(metrics.widthPixels / 4, 0,metrics.widthPixels / 4, 0);
+            cardsList.setColumnWidth(metrics.widthPixels / 2);
+        }
         CardGridAdapter adapter = new CardGridAdapter(this, R.layout.card_results, selectedCards);
         cardsList.setAdapter(adapter);
     }
@@ -68,5 +87,33 @@ public class ShowCardsActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    private float x1,x2;
+    static final int MIN_DISTANCE = 150;
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        switch(event.getAction())
+        {
+            case MotionEvent.ACTION_DOWN:
+                x1 = event.getX();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = event.getX();
+                float deltaX = x2 - x1;
+                if (Math.abs(deltaX) > MIN_DISTANCE)
+                {
+                    Toast.makeText(this, "left2right swipe", Toast.LENGTH_SHORT).show ();
+                }
+                else
+                {
+                    // consider as something else - a screen tap for example
+                }
+                break;
+        }
+        return super.onTouchEvent(event);
     }
 }
